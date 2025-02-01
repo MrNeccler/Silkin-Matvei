6f11de1fa73b40f8d83ad25fc478c1b56612f666
MrNeccler <147950148+MrNeccler@users.noreply.github.com>
2025-02-01 15:13:50 +0300

    Update pz 5.py

--- Manakova/pz 5.py
+++ Manakova/pz 5.py
@@ -2,21 +2,24 @@
 определяет, есть ли в числе одинаковые цифры.'''
 import random
 
-def фунцкия():
+
+def pr():
     # Генерируем четырехзначное число
     number = random.randint(1000, 9999)
     return number
 
-def has_duplicate_digits(number):
+
+def has(number):
     # Преобразуем число в строку и создаем множество для проверки уникальности
     digits = str(number)
     return len(set(digits)) < len(digits)
 
+
 def main():
-    number = функция()
+    number = pr()
     print(f"Сгенерированное число: {number}")
-    
-    if has_duplicate_digits(number):
+
+    if has(number):
         print("В числе есть одинаковые цифры.")
     else:
         print("В числе нет одинаковых цифр.")
